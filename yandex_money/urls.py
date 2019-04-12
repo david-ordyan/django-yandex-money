73ee7e1fd405e94c76a8449308731663a905f08a
Xrizolin <Xrizolin@users.noreply.github.com>
2019-04-12 15:05:51 +0300

    Update urls.py

--- yandex_money/urls.py
+++ yandex_money/urls.py
@@ -5,7 +5,6 @@ from .views import NoticeFormView
 from .views import CheckOrderFormView
 
 
-urlpatterns = patterns('',
-    url(r'^check/', CheckOrderFormView.as_view(), name='yandex_money_check'),
-    url(r'^notice/', NoticeFormView.as_view(), name='yandex_money_notice'),
-)
+urlpatterns = [url(r'^check/', CheckOrderFormView.as_view(), name='yandex_money_check'),
+               url(r'^notice/', NoticeFormView.as_view(), name='yandex_money_notice')
+]
